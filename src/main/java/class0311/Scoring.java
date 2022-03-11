36ca4d43dc64e28dab419f4878da483b23da44b5
ciao409 <93966235+ciao409@users.noreply.github.com>
2022-03-11 18:17:40 +0800

    Create Scoring.java

---
+++ src/main/java/class0311/Scoring.java
@@ -0,0 +1,13 @@
+package class0311;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack = new Student( "Jack", 49, 64);
+        Student sylvie =new Student("Joy", 85, 76);
+        GraduateStudent jane =
+                new GraduateStudent("Jane", 64, 70, 83);
+        jack.print();
+        joy.print();
+        jane.print();
+    }
+}
