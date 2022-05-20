b22c69f20777051ac453b7c10d2714bd071de628
ciao409 <yuciao276@gmail.com>
2022-05-20 12:19:35 +0800

    reorganize

---
+++ src/main/java/Ticket/Tester.java
@@ -0,0 +1,60 @@
+package Ticket;
+
+import java.util.Scanner;
+
+public class Tester  {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your start station?(1.Taipei 2.Taichung 3.Kaohsiung)");
+        Station start = null;
+        int choice = Integer.parseInt(scanner.next());
+        switch (choice){
+            case 1:
+                start = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                start = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                start = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        System.out.println("Your end station?(1.Taipei 2.Taichung 3.Kaohsiung)");
+        Station destination = null;
+        choice= Integer.parseInt(scanner.next());
+        switch (choice){
+            case 1:
+                destination = Station.TAIPEI_STATION;
+                break;
+            case 2:
+                destination = Station.TAICHUNG_STATION;
+                break;
+            case 3:
+                destination = Station.KAOHSIUNG_STATION;
+                break;
+        }
+        System.out.println("Which kind of ticket? 1)Normal Ticket 2)Student Ticket 3)Elder Ticket");
+        choice = Integer.parseInt(scanner.next());
+        Ticket ticket = null;
+        switch (choice) {
+            case 1:
+                ticket = new Ticket(start, destination);
+                System.out.print("Normal Ticket ");
+                break;
+            case 2:
+                ticket = new StudentTicket(start, destination);
+                System.out.print("StudentTicket ");
+                break;
+            case 3:
+                ticket = new ElderTicket(start, destination);
+                System.out.print("ElderTicket ");
+                break;
+        }
+        ticket.print();
+
+    }
+
+
+
+
+}
