52319f8b9ad45aff55723aae161ae7d8bd80719e
ciao409 <yuciao276@gmail.com>
2022-05-20 16:28:24 +0800

    reorganize

---
+++ src/main/java/UI/MyWindows.java
@@ -0,0 +1,41 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindows {
+    public static void main(String[] args) {
+        //swing,SWT,JavaFX
+        //eXtension
+        JFrame frame=new JFrame();
+        frame.setSize(600,400);
+        frame.setLocation(300,200);
+        frame.getDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //odd Component
+        JButton button=new JButton("ok");
+        //匿名類別，Anonymous class
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                System.out.println("Hello world!");
+            }
+        });
+        //MyActionListener Listener=new MyActionListener();
+        //button.addActionListener(Listener);
+        //Layout
+        //frame.setLayout(new BorderLayout());
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(new JButton("Testing"));
+        frame.add(new JButton("Testing"));
+        frame.add(new JButton("Testing"));
+        frame.add(new JButton("Testing"));
+        frame.add(new JButton("Testing"));
+        frame.add(new JButton("Testing"));
+        frame.setVisible(true);
+
+        System.out.println("END?");
+    }
+}
